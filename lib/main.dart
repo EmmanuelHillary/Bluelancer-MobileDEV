67b1510f687ea4b2b736afc46f4f776ccd82f37a
Omasyo <cesare3000@outlook.com>
2022-08-12 04:55:18 +0100

    fixed theme

--- lib/main.dart
+++ lib/main.dart
@@ -16,65 +16,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // primarySwatch: Colors.amber,
-        // colorScheme: ColorScheme.fromSeed(
-        //   seedColor: const Color.fromRGBO(47, 84, 235, 1),
-        // ),
-        colorSchemeSeed: const Color.fromRGBO(47, 84, 235, 1),
-        // fontFamily: GoogleFonts.dmSans().fontFamily,
-        textTheme: GoogleFonts.dmSansTextTheme().copyWith(
-          button: const TextStyle(
-            fontWeight: FontWeight.w500,
-            fontSize: 16,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.all(16),
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(8)),
-            ),
-          ),
-        ),
-        textButtonTheme: TextButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.all(16),
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(8)),
-            ),
-          ),
-        ),
-        // useMaterial3: true,
-      ),
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: ColorScheme.fromSeed(
-          brightness: Brightness.dark,
-          seedColor: const Color.fromRGBO(47, 84, 235, 1),
-        ),
-        textTheme: GoogleFonts.dmSansTextTheme().copyWith(
-          labelLarge: const TextStyle(
-            fontWeight: FontWeight.w500,
-            fontSize: 16,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.all(16),
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(8)),
-            ),
-          ),
-        ),
-        textButtonTheme: TextButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.all(16),
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(8)),
-            ),
-          ),
-        ),
-      ),
+      theme: BlueLancerTheme.lightTheme,
+      darkTheme: BlueLancerTheme.darkTheme,
+      // theme.copyWith(
+      //   colorScheme:
+      //   ColorScheme.fromSeed(
+      //     seedColor: Color.fromRGBO(47, 84, 235, 1),
+      //     brightness: Brightness.light,
+      //   ),
+      // ),
+      //     ThemeData.dark().copyWith(
+      //   colorScheme: ColorScheme.fromSeed(
+      //     brightness: Brightness.dark,
+      //     seedColor: const Color.fromRGBO(47, 84, 235, 1),
+      //   ),
+      //   textTheme: GoogleFonts.dmSansTextTheme().copyWith(
+      //     labelLarge: const TextStyle(
+      //       fontWeight: FontWeight.w500,
+      //       fontSize: 16,
+      //     ),
+      //   ),
+      //   elevatedButtonTheme: ElevatedButtonThemeData(
+      //     style: ElevatedButton.styleFrom(
+      //       padding: const EdgeInsets.all(16),
+      //       shape: const RoundedRectangleBorder(
+      //         borderRadius: BorderRadius.all(Radius.circular(8)),
+      //       ),
+      //     ),
+      //   ),
+      //   textButtonTheme: TextButtonThemeData(
+      //     style: ElevatedButton.styleFrom(
+      //       padding: const EdgeInsets.all(16),
+      //       shape: const RoundedRectangleBorder(
+      //         borderRadius: BorderRadius.all(Radius.circular(8)),
+      //       ),
+      //     ),
+      //   ),
+      // ),
+
       home: const OnboardingScreen(),
       routes: Routes.routes,
       // routes: ,
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,63 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-var theme = ThemeData(
-  // primarySwatch: Colors.amber,
-  // colorScheme: ColorScheme.fromSeed(
-  //   seedColor: const Color.fromRGBO(47, 84, 235, 1),
-  // ),
-  colorSchemeSeed: const Color.fromRGBO(47, 84, 235, 1),
-  // fontFamily: GoogleFonts.dmSans().fontFamily,
-  textTheme: GoogleFonts.dmSansTextTheme().copyWith(
+abstract class BlueLancerTheme {
+  static final textTheme =
+      GoogleFonts.dmSansTextTheme(const TextTheme()).copyWith(
     button: const TextStyle(
       fontWeight: FontWeight.w500,
       fontSize: 16,
     ),
-  ),
-  elevatedButtonTheme: ElevatedButtonThemeData(
-    style: ElevatedButton.styleFrom(
-      padding: const EdgeInsets.all(16),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(8)),
-      ),
-    ),
-  ),
-  textButtonTheme: TextButtonThemeData(
-    style: ElevatedButton.styleFrom(
-      padding: const EdgeInsets.all(16),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(8)),
-      ),
-    ),
-  ),
-);
+  );
 
-var ll = TextTheme();
+  static final elevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      padding: const EdgeInsets.all(16),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(8)),
+      ),
+    ),
+  );
 
-var darkTheme = ThemeData.dark().copyWith(
-  colorScheme: ColorScheme.fromSeed(
-    seedColor: const Color.fromRGBO(47, 84, 235, 1),
-  ),
-  textTheme: GoogleFonts.dmSansTextTheme().copyWith(
-    button: const TextStyle(
-      fontWeight: FontWeight.w500,
-      fontSize: 16,
-    ),
-  ),
-  elevatedButtonTheme: ElevatedButtonThemeData(
+  static final textButtonTheme = TextButtonThemeData(
     style: ElevatedButton.styleFrom(
       padding: const EdgeInsets.all(16),
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(8)),
       ),
     ),
-  ),
-  textButtonTheme: TextButtonThemeData(
-    style: ElevatedButton.styleFrom(
-      padding: const EdgeInsets.all(16),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(8)),
-      ),
+  );
+
+  static final lightTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color.fromRGBO(47, 84, 235, 1),
+      brightness: Brightness.light,
     ),
-  ),
-);
+    textTheme: textTheme,
+    elevatedButtonTheme: elevatedButtonTheme,
+    textButtonTheme: textButtonTheme,
+    // useMaterial3: true,
+  );
+
+  static final darkTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color.fromRGBO(47, 84, 235, 1),
+      brightness: Brightness.dark,
+    ),
+    // fontFamily: GoogleFonts.dmSans().fontFamily,
+    textTheme: textTheme,
+    elevatedButtonTheme: elevatedButtonTheme,
+    textButtonTheme: textButtonTheme,
+    // useMaterial3: true,
+  );
+}
